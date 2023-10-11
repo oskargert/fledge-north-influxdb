774a1d9622ec773a3779d3221644312dce471fae
ogert <oskar.gert@acdp.at>
2023-10-11 09:37:16 +0200

    add logger to north plugin

--- plugin_test/plugin_test.py
+++ plugin_test/plugin_test.py
@@ -1,17 +1,17 @@
-import json, asyncio
+import json, asyncio, logging
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'python', 'fledge', 'plugins', 'north', 'influxdb_python'))
 from InfluxDBNorthPlugin import InfluxDBNorthPlugin
 
 if __name__ == "__main__":
-
+	logger = logging.getLogger(__name__)
 	with open(os.path.join('plugin_test', 'example_config.json')) as f:
 		config = json.load(f)
 	with open(os.path.join('plugin_test','example_data.json')) as f:
 		data = json.load(f)
 
-	influxclient = InfluxDBNorthPlugin(config)
+	influxclient = InfluxDBNorthPlugin(config, logger)
 	asyncio.run(influxclient.send_payloads(data))
 	
 	print("Done")
